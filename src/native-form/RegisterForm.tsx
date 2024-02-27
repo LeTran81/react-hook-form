3b2c6fd8f556501e76913cc82b013d60d85045ce
letran-ml <le.tran@decurret-dcp.com>
2024-02-27 07:59:58 +0700

    register form input native

--- src/native-form/RegisterForm.tsx
+++ src/native-form/RegisterForm.tsx
@@ -1,64 +1,57 @@
-import React, { useState, ChangeEvent, FormEvent } from "react";
+import React from "react";
 import "./RegisterForm.css"; // Import the CSS file for styling
+import { useForm } from "react-hook-form";
 
-interface FormData {
-  name: string;
+interface Values {
+  userName: string;
   email: string;
   password: string;
 }
 
 const RegisterForm: React.FC = () => {
-  const [formData, setFormData] = useState<FormData>({
-    name: "",
-    email: "",
-    password: "",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Values>({
+    defaultValues: {
+      userName: "",
+      email: "",
+      password: "",
+    },
   });
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    // Handle form submission here, e.g., send data to server
-    console.log(formData);
-    // You can add your logic to handle form submission, e.g., sending data to a backend server
+  const onSubmit = (data: Values) => {
+    console.log(data);
   };
 
   return (
     <div className="register-form-container">
       <h2>Register</h2>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-group">
           <label>Name</label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            required
-          />
+          <input type="text" {...(register("userName"), { required: true })} />
+          {errors.userName && <span>This field is required</span>}
         </div>
         <div className="form-group">
           <label>Email</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            required
-          />
+          <input type="email" {...(register("email"), { required: true })} />
+          {errors.email && <span>This field is required</span>}
         </div>
         <div className="form-group">
           <label>Password</label>
           <input
             type="password"
-            name="password"
-            value={formData.password}
-            onChange={handleChange}
-            required
+            {...(register("password"),
+            { required: true, minLength: 6, maxLength: 12 })}
           />
+          {errors.password && errors.password.type === "required" && (
+            <span>This field is required</span>
+          )}
+          {errors.password && errors.password.type === "minLength" && (
+            <span>Password must have at least 6 characters</span>
+          )}
         </div>
         <button type="submit">Register</button>
       </form>
